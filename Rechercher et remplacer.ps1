15a0a5268b575513549e39bd542affa9ac565c9c
vince1016 <45361827+vince1016@users.noreply.github.com>
2018-12-18 00:15:04 -0500

    Revert "v1.0001"
    This reverts commit c34bc8bdb9d237f6cb3098c31919caf602f420cb.

--- Rechercher et remplacer.ps1
+++ Rechercher et remplacer.ps1
@@ -1,9 +1,6 @@
 $Rechercher = $Args[0]
 $Changer = $Args[1]
 $fichiers = get-childItem -include *.txt, *.csv, *.doc, -recurse
-forEach(){
-    
-}
 $message = read-host -prompt "remplacer la chaine $Changer avec la valeur placer dans le fichier $fichiers (o/n)"
 
 
--- search_event.ps1
+++ search_event.ps1
@@ -1,4 +1,1 @@
-Get-EventLog -List 
-Get-EventLog Application >> Logs.txt
-Get-Content Logs.txt | where { $_ -like "**" }
-Get-ChildItem -Recurse -Include.
+get
