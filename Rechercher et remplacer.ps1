5f97d2832dc9a044c3f3250e5d84efaa226969ea
vince1016 <45361827+vince1016@users.noreply.github.com>
2018-12-17 21:48:15 -0500

    marche pas encore(backp)

--- Rechercher et remplacer.ps1
+++ Rechercher et remplacer.ps1
@@ -1,1 +1,17 @@
-get
+$Rechercher = $Args[0]
+$Changer = $Args[1]
+$fichiers = get-childItem -include *.txt, *.csv, *.doc, -recurse
+$message = read-host -prompt "remplacer la chaine $Changer avec la valeur placer dans le fichier $fichiers (o/n)"
+
+
+
+if ($message -eq "o")
+{
+$content = Get-Content "fichiers\texte\texte.txt" | foreach { $_ -replace $Rechercher,$Changer }
+Set-Content -path $fichiers -Value $content
+Write-Output "Le remplacement a corectement ete effectue."
+}
+else{
+    Write-Output "Aucun changement n'a ete aporter"
+}
+
